821066f15162bdd7be30e45cf5cfbf042c97943c
Haniya <haniyasajjad18@gmail.com>
2025-04-28 23:24:12 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.fast.assignment3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.fast.assignment3"
+        minSdk = 34
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.glide)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.messaging.ktx)
+    implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.onesignal:OneSignal:[5.1.6, 5.1.99]")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.firebase:firebase-messaging:23.1.2")
+    implementation("io.agora.rtc:full-sdk:4.4.0")
+
+}
